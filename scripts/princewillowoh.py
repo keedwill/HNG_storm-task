bbe1e15c0c80fd6a09532fe5ee5f6d3888737ef9
keedwill <60316863+keedwill@users.noreply.github.com>
2020-06-05 08:10:05 +0100

    Create princewillowoh.py

---
+++ scripts/princewillowoh.py
@@ -0,0 +1,11 @@
+greeting = "Hello world"
+first_name = "princewill"
+last_name = "owoh"
+HNGi7_ID =  "HNG-04232"
+language = "Python"
+
+def script():
+    result = f'{greeting}, this is {first_name} {last_name} with HNGi7_ID {HNGi7_ID} using {language} for stage2 Task'
+    return result
+
+print(script())
